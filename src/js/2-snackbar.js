375ef0927d5292507c6aa97fe17e5325de558836
JaneBee24 <bublikkjenya@gmail.com>
2025-03-02 06:06:11 +0100

    8

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,32 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const delay = Number(form.elements.delay.value);
+    const state = form.elements.state.value;
+
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    })
+        .then((delay) => {
+            iziToast.success({
+                message: `✅ Fulfilled promise in ${delay}ms`,
+                position: "topRight",
+                timeout: 5000,
+                close: true,
+                backgroundColor: "#4CAF50"
+            });
+        })
+        .catch((delay) => {
+            iziToast.error
+        });
+    })
